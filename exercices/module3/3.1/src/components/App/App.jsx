95801bbd90c81aeb5491f2a7791dbe14ec6b5b82
NJKatorz <nathanesakwa@gmail.com>
2024-07-07 14:34:17 +0200

    module3 : loading

--- exercices/module3/3.1/src/components/App/App.jsx
+++ exercices/module3/3.1/src/components/App/App.jsx
@@ -2,12 +2,18 @@ import './App.css'
 import Button from 'components/Button/Button'
 import { useState } from 'react'
 import Statistics from 'components/Statistics/Statistics'
+import Loading from 'components/Loading/Loading'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  setTimeout(() => {
+    setLoading(false)
+  }, 3000);
+
 
   const handleClick = (e) => {
     const val = e.target.className;
@@ -20,6 +26,9 @@ const App = () => {
     }
   }
 
+  if (loading) {
+    return <Loading />;
+  }
 
   
   return (
