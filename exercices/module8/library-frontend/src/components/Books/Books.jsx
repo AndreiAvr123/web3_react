c13205e765c56815f84d4790a7c05586b6612349
NJKatorz <nathanesakwa@gmail.com>
2024-07-31 21:01:01 +0200

    module 8 : graphQL frontend

---
+++ exercices/module8/library-frontend/src/components/Books/Books.jsx
@@ -0,0 +1,39 @@
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../../graphql-queries'
+
+function Books(props) {
+
+  const result = useQuery(ALL_BOOKS);
+  
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>books</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {result.data.allBooks.map((book) => (
+          <tr key={book.id}>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.published}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
